e33dfbc3cce946ac4331b946e5ec20a5196ced87
dawid białek <dawid1bialek@gmail.com>
2022-09-29 22:38:18 +0200

    Add model value

--- CurrencyCalculator/src/main/java/com/example/CurrencyCalculator/CurrencyModel.java
+++ CurrencyCalculator/src/main/java/com/example/CurrencyCalculator/CurrencyModel.java
@@ -1,4 +1,24 @@
 package com.example.CurrencyCalculator;
 
 public class CurrencyModel {
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    private double rate;
+    private String currency;
+
 }
