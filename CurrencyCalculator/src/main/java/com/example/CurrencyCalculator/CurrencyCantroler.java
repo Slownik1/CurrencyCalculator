2e3104e05d87d68b9bb01f811e4aa49faac9e69d
dawid białek <dawid1bialek@gmail.com>
2022-09-29 22:36:19 +0200

    Add "/" endpoint

--- CurrencyCalculator/src/main/java/com/example/CurrencyCalculator/CurrencyCantroler.java
+++ CurrencyCalculator/src/main/java/com/example/CurrencyCalculator/CurrencyCantroler.java
@@ -1,4 +1,20 @@
 package com.example.CurrencyCalculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CurrencyCantroler {
+
+    private final CurrencyService currencyService;
+
+    public CurrencyCantroler(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    @GetMapping("/")
+    public CurrencyModel getCurrencyValue(){
+        return currencyService.getCurencyValue();
+    }
+
 }
--- CurrencyCalculator/src/main/java/com/example/CurrencyCalculator/CurrencyService.java
+++ CurrencyCalculator/src/main/java/com/example/CurrencyCalculator/CurrencyService.java
@@ -1,4 +1,21 @@
 package com.example.CurrencyCalculator;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Slf4j
 public class CurrencyService {
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public CurrencyModel getCurencyValue(){
+
+        String response = restTemplate.getForObject("http://api.nbp.pl/api/exchangerates/rates/A/CHF/", String.class);
+        log.info(response);
+        return null;
+
+    }
+
 }
